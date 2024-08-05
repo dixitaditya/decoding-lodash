0d4d20e8a0aca0bef1fe16e9cce9245b28959766
Aditya Dixit <dixitaditya12@gmail.com>
2024-08-05 16:07:53 +0530

    uniq polyfill - beginner

---
+++ uniq/beginner.js
@@ -0,0 +1,9 @@
+function _uniq(array) {
+    if (!Array.isArray(array)) {
+      throw new TypeError('Expected an array');
+    }
+    
+    // Create a Set from the array to remove duplicates
+    return Array.from(new Set(array));
+  }
+  
