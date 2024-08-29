4ca85b10c937df88e6594eacbbb9f9f7b38efa2f
Aditya Dixit <dixitaditya12@gmail.com>
2024-08-29 17:51:48 +0530

    find polyfill (#14)

---
+++ find/base.js
@@ -0,0 +1,15 @@
+function _find(array, predicate) {
+    if (!Array.isArray(array)) {
+      throw new TypeError('Expected an array');
+    }
+    if (typeof predicate !== 'function') {
+      throw new TypeError('Expected a predicate function');
+    }
+  
+    for (let i = 0; i < array.length; i++) {
+      if (predicate(array[i], i, array)) {
+        return array[i];
+      }
+    }
+    return undefined;
+  }
